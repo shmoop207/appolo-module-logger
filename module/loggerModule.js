6ed7330c5d6057181799aff22c71fbf5f18add53
roman <shmoop207@gmail.com>
2020-03-27 14:32:39 +0300

    appolo bump

--- module/loggerModule.js
+++ module/loggerModule.js
@@ -1,15 +1,19 @@
 "use strict";
+var LoggerModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const index_1 = require("appolo/index");
 const logger_1 = require("./src/logger");
-let LoggerModule = class LoggerModule extends index_1.Module {
+let LoggerModule = LoggerModule_1 = class LoggerModule extends index_1.Module {
     constructor(opts) {
         super(opts);
         this.Defaults = {};
     }
+    static for(opts) {
+        return new LoggerModule_1(opts);
+    }
 };
-LoggerModule = tslib_1.__decorate([
+LoggerModule = LoggerModule_1 = tslib_1.__decorate([
     index_1.module({ exports: [logger_1.Logger], immediate: true })
 ], LoggerModule);
 exports.LoggerModule = LoggerModule;
--- module/loggerModule.ts
+++ module/loggerModule.ts
@@ -6,11 +6,13 @@ import {IOptions} from "../index";
 @module({exports: [Logger], immediate: true})
 export class LoggerModule extends Module<IOptions> {
 
-    protected readonly Defaults: Partial<IOptions> = {
-
-    };
+    protected readonly Defaults: Partial<IOptions> = {};
 
     constructor(opts?: IOptions) {
         super(opts);
+    }
+
+    public static for(opts?: IOptions): LoggerModule {
+        return new LoggerModule(opts)
     }
 }
